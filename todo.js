ebd9e0965077e8091a6129b6d1b74a98519852b8
aadaij <117342970+aadaij@users.noreply.github.com>
2023-04-11 17:59:28 +0200

    working to do list with quote

--- todo.js
+++ todo.js
@@ -8,7 +8,6 @@ const titlePopUpWindowElement = document.getElementById("titlePopUpWindow");
 const inputFieldElement = document.getElementById("inputField");
 const listContainerElement = document.getElementById("listContainer");
 const titelInputFieldElement = document.getElementById("inputFieldTitle");
-const titleContainerElement = document.getElementById("titleContainer");
 const headingElement = document.getElementById("heading");
 
 // floaring action buttons
@@ -19,24 +18,26 @@ plusSignElement.addEventListener("click", () => {
   plusSignElement.classList.toggle("fab-content-after");
 });
 
-// clicking on adding a task opens the task adding window
+// clicking on "adding a task" opens the task adding pop up window
 taskElement.addEventListener("click", () => {
   containerElement.classList.toggle("blur");
   popUpWindowElement.classList.toggle("pop-up-open");
 });
 
-// clicking on the add a title task opens the title adding window
+// clicking on the "add a title" task opens the title adding pop up window
 titleElement.addEventListener("click", () => {
   containerElement.classList.toggle("blur");
   titlePopUpWindowElement.classList.toggle("pop-up-open");
 });
 
-// input frame for adding a task
+// input frame for adding a task (called when clicking the "add" button)
 function addingTask() {
   if (inputFieldElement.value === "") {
+    // if the input field is empty just close the pop up window
     containerElement.classList.remove("blur");
     popUpWindowElement.classList.remove("pop-up-open");
   } else {
+    // if there is input, create a list item with the removing x symbol to the list
     let li = document.createElement("li");
     li.innerHTML = inputFieldElement.value;
     listContainerElement.appendChild(li);
@@ -45,29 +46,33 @@ function addingTask() {
     deleteCros.src = "images/lil-x.svg";
     li.appendChild(deleteCros);
 
+    // closing the pop up
     containerElement.classList.remove("blur");
     popUpWindowElement.classList.remove("pop-up-open");
 
     saveTheTasks();
   }
+  // empty input value when returning to the imput field
   inputFieldElement.value = "";
 }
 
-// input frame for adding a title
+// input frame for adding a title (called when clicked "save" button)
 function addingTitle() {
   if (titelInputFieldElement.value === "") {
-    titleContainerElement.classList.remove("blur");
+    // if the input field is empty just close the pop up window
     containerElement.classList.remove("blur");
     titlePopUpWindowElement.classList.remove("pop-up-open");
   } else {
+    // make the imput the title
     headingElement.innerText = titelInputFieldElement.value;
 
-    titleContainerElement.classList.remove("blur");
+    // closing the pop up
     containerElement.classList.remove("blur");
     titlePopUpWindowElement.classList.remove("pop-up-open");
 
     saveTheTasks();
   }
+  // empty input value when returning to the imput field
   titelInputFieldElement.value = "";
 }
 
@@ -83,7 +88,7 @@ listContainerElement.addEventListener("click", function (e) {
   }
 });
 
-// saving tasks to local storage
+// saving tasks and the title to local storage
 function saveTheTasks() {
   localStorage.setItem("tasks", listContainerElement.innerHTML);
   localStorage.setItem("title", headingElement.innerHTML);
@@ -92,6 +97,7 @@ function saveTheTasks() {
 // getting the saved information form local storage
 function getTasks() {
   listContainerElement.innerHTML = localStorage.getItem("tasks");
+
   if (localStorage.getItem("title") === null) {
     headingElement.innerHTML = "to do list";
   } else {
@@ -100,37 +106,29 @@ function getTasks() {
 }
 getTasks();
 
-// localStorage.clear();
+// source for the quote api https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
+// source for how to link the api https://www.youtube.com/watch?v=lpuBLpwgpp8&t=420s
+function onLoad() {
+  fetch("https://type.fit/api/quotes")
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      const randomQuote = response[Math.floor(Math.random() * 1643)];
+      const quote = randomQuote.text;
+      const author = randomQuote.author;
 
-// function onLoad() {
-//   fetch("https://today.zenquotes.io/api/1/1").then((response) => {
-//     console.log(response);
-//   });
-// }
+      const quoteElement = document.getElementById("inspirationalQuote");
+      const authorElement = document.getElementById("quoteAuthor");
 
-// window.addEventListener("load", onLoad);
+      quoteElement.innerText = quote;
 
-function loadFox() {
-  const url = "https://zenquotes.io/api/image";
-  const method = "GET";
-
-  const foxRequest = new XMLHttpRequest();
-  foxRequest.responseType = "json";
-  foxRequest.open(method, url);
-  foxRequest.addEventListener("load", finishedLoadingHandler);
-  foxRequest.send();
-  console.log(foxRequest);
+      if (author === null) {
+        authorElement.innerText = "- Unknown";
+      } else {
+        authorElement.innerText = "- " + author;
+      }
+    });
 }
 
-function finishedLoadingHandler(event) {
-  const finishedRequest = event.target;
-  const response = finishedRequest.response;
-  const foxUrl = response.image;
-
-  const imgElement = document.createElement("img");
-  imgElement.src = foxUrl;
-
-  const bodyElement = document.querySelector("body");
-  bodyElement.appendChild(imgElement);
-}
-loadFox();
+window.addEventListener("load", onLoad);
